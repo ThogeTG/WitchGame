bc5cc568a05d9db9199ee0bec525ef98eb207b6e
Kunnari Jalmari TTK17SP <jalmarikunnari@kamk.fi>
2018-01-17 11:48:29 +0200

    Villain script added, not yet completed.

---
+++ Assets/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public Rigidbody2D myRB;
+    public Transform player;
+    public float thrust;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        myRB = gameObject.GetComponent<Rigidbody2D>();
+
+       myRB.AddForce(transform.up * thrust);
+
+        myRB.AddForceAtPosition(player.transform.position, player.transform.position * thrust);
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+
+		
+	}
+
+    private void FixedUpdate()
+    {
+        if (transform.position 
+        {
+
+        }
+
+
+    }
+}
