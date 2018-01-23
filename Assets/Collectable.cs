de434a9bafda2e2956ca673c3897471204a7ed33
Kinnunen Eero TTK17SP <eerokinnunen@kamk.fi>
2018-01-23 14:08:44 +0200

    juttui

---
+++ Assets/Collectable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour {
+
+    public bool grounded;
+    public Rigidbody2D myRb;
+
+	// Use this for initialization
+	void Start () {
+        myRb = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        grounded = Physics2D.Linecast(transform.position, new Vector2(transform.position.x, transform.position.y - 0.25f), 1 << LayerMask.NameToLayer("Ground"));
+        if (grounded)
+        {
+            myRb.AddForce(new Vector2(0, 50));
+        }
+    }
+}
