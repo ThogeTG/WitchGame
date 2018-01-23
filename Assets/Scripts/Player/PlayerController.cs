d825422467a46ff1c64cacc0a9880ceebd5f2453
Niininen Touko TTK17SP <toukoniininen@kamk.fi>
2018-01-23 15:26:23 +0200

    Shooting mecanism and player sprite flip added.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 10f;
+    [SerializeField]
+    private float moveSpeed = 10f;
+    [SerializeField]
+    private float jumpSpeed;
 
     private Rigidbody2D rb;
-    private Animator anim;
+
+    float h;
+    float v;
 
     private bool jump = false;
     [SerializeField]
@@ -17,36 +22,75 @@ public class PlayerController : MonoBehaviour
 
     private Vector3 moveDir;
 
+    public GameObject fireBall;
+    [SerializeField]
+    int fireBallSpeed;
+
+    SpriteRenderer sr;
+
+    public bool lookRight;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+
+        lookRight = true;
+
         //anim = GetComponent<Animator>();
+        //playerFind = GameObject.FindGameObjectWithTag("Player");
     }
 
     private void FixedUpdate()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
 
         moveDir = new Vector2(h, 0);
 
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            jump = true;
+        }
+
         if (jump == true)
         {
             //rb.AddForce(new Vector2(0f, jumpForce));
-            rb.velocity = new Vector2(0, 6);
+            rb.velocity = new Vector2(0, jumpSpeed);
             jump = false;
         }
     }
 
     void Update()
     {
+        if (h > 0.1f)
+        {
+            sr.flipX = false;
+            lookRight = true;
+        }
+        if (h < -0.1f)
+        {
+            sr.flipX = true;
+            lookRight = false;
+        }
+
         grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
 
-        if (Input.GetButtonDown("Jump") && grounded)
+        if (Input.GetButtonDown("Fire1"))
         {
-            jump = true;
+
+            if (lookRight == true)
+            {
+                GameObject spawned = Instantiate(fireBall, transform.position, transform.rotation);
+                spawned.GetComponent<Rigidbody2D>().AddForce(new Vector2(fireBallSpeed, 0));
+            }
+            if (lookRight == false)
+            {
+                GameObject spawned = Instantiate(fireBall, transform.position, transform.rotation);
+                spawned.GetComponent<Rigidbody2D>().AddForce(new Vector2(-fireBallSpeed, 0));
+            }
         }
     }
 }
