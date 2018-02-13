d89c458a62f6072bc92dfbf48e1a5f11c473a3a4
Kinnunen Eero TTK17SP <eerokinnunen@kamk.fi>
2018-02-13 14:23:51 +0200

    Collectable system WIP

--- Assets/Collectable.cs
+++ Assets/Collectable.cs
@@ -6,6 +6,7 @@ public class Collectable : MonoBehaviour {
 
     public bool grounded;
     public Rigidbody2D myRb;
+    public bool canCollect = false;
 
 	// Use this for initialization
 	void Start () {
@@ -20,10 +21,16 @@ public class Collectable : MonoBehaviour {
         //    myRb.AddForce(new Vector2(0, 50));
         //}
 
-        grounded = Physics2D.Linecast(transform.position, new Vector2(transform.position.x, transform.position.y - 0.4f), 1 << LayerMask.NameToLayer("Collectables"));
+        grounded = Physics2D.Linecast(transform.position, new Vector2(transform.position.x, transform.position.y - 0.245f), 1 << LayerMask.NameToLayer("Ground"));
         if (grounded)
         {
-            myRb.AddForce(new Vector2(5, 0));
+            myRb.velocity = new Vector2(myRb.velocity.x, 0);
+            myRb.gravityScale = 0;
+            canCollect = true;
+        }
+        else
+        {
+            myRb.gravityScale = 1;
         }
     }
 }
---
+++ Assets/CollectableSpa.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableSpa : MonoBehaviour {
+
+    public GameObject pressButt;
+    public GameObject toShow;
+    public Transform player;
+    public SpriteRenderer toShowRend;
+    public GameObject collectablePre;
+
+    public Sprite colSprite;
+
+    // Use this for initialization
+    private void Awake()
+    {
+        toShow = Instantiate(pressButt, new Vector3(transform.position.x, transform.position.y + 3, transform.position.z), Quaternion.identity);
+        toShowRend = toShow.GetComponent<SpriteRenderer>();
+        toShowRend.enabled = false;
+    }
+
+    void Start ()
+    {
+        player = GameObject.Find("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Vector3.Distance(transform.position, player.position) <= 2)
+        {
+            toShowRend.enabled = true;
+            if(Input.GetButtonDown("Fire1"))
+            {
+                GameObject spawned;
+                spawned = Instantiate(collectablePre, new Vector2(transform.position.x, transform.position.y +1), Quaternion.identity);
+                spawned.transform.Find("Sprite").GetComponent<SpriteRenderer>().sprite = colSprite;
+                spawned.GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(-3.0f, 3.0f), Random.Range(4.0f, 6.0f)); 
+            }
+        }
+        else
+        {
+            toShowRend.enabled = false;
+        }
+	}
+}
